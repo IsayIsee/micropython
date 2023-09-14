16dd5893187bffa4e01182fc0d77fa4be206a0f0
Zhan Kui <zhankui@ai-pu.com>
2023-09-14 11:53:16 +0800

    * add froze config for pico * froze some useful lib * change USB MANUFACTURER and PRODUCT string

--- ports/rp2/boards/RPI_PICO/mpconfigboard.h
+++ ports/rp2/boards/RPI_PICO/mpconfigboard.h
@@ -1,3 +1,11 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+#define MICROPY_HW_USB_MANUFACTURER_STRING      "IC"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING        "CardReader"
+#define MICROPY_HW_USB_CDC_INTERFACE_STRING     "Board CDC"
+
+//#define MICROPY_ENABLE_COMPILER                 (0)
+//#define MICROPY_PY_BUILTINS_COMPILE             (0)
+//#define MICROPY_PY_BUILTINS_EXECFILE            (0)
