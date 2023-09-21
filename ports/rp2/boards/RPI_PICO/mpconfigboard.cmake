c88c8665a8c9cbec3f2d9d687b3ec60d351947dc
Zhan Kui <zhankui@ai-pu.com>
2023-09-21 14:49:21 +0800

    * add W5500 network support

--- ports/rp2/boards/RPI_PICO/mpconfigboard.cmake
+++ ports/rp2/boards/RPI_PICO/mpconfigboard.cmake
@@ -1,5 +1,8 @@
 # cmake file for Raspberry Pi Pico
 set(PICO_BOARD "pico")
 
+set(MICROPY_PY_NETWORK_WIZNET5K W5500)
+set(MICROPY_PY_LWIP 1)
+
 # Board specific version of the frozen manifest
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
